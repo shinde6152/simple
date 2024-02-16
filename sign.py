292080aab71a6733ab7f73f40f4a2361966e82f9
shinde6152 <153020547+shinde6152@users.noreply.github.com>
2024-02-17 01:10:48 +0530

    Signup model of user

--- sign.py
+++ sign.py
@@ -1,27 +1,9 @@
-from django.shortcuts import render,redirect
-from store.models.product import product
-from store.models.category import category
-from django.views import View
-from store.models.sign import sign
+from django.db import models
 
+class sign(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=30)
 
-class signup(View):
-    def get(self,request):
-        return render(request,'sign.html')
-
-
-    def post(self,request):
-        try:
-            if request.method == 'POST':
-                name = request.POST.get('name')
-                email = request.POST.get('email')
-                password = request.POST.get('password')
-                print(name, email, password)
-
-                sign_obj = sign(name=name, email=email, password=password)
-                sign_obj.save()
-        except:
-            pass
-
-
-        return redirect("login")
+    def __str__(self):
+        return self.name
