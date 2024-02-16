8e16efded2ab64c42ea72db527bf76131b6ae55a
shinde6152 <153020547+shinde6152@users.noreply.github.com>
2024-02-17 01:15:48 +0530

    admin login data

--- admin_login.py
+++ admin_login.py
@@ -1,23 +1,10 @@
-from django.shortcuts import render,redirect
-from django.views import View
-from store.models.admin_login import admin_login
-
-class admin_log(View):
-    def get(self,request):
+from django.db import models
 
 
-        return render(request,'admin_login.html')
-    def post(self,request):
-        email=request.POST.get('email')
-        password=request.POST.get('password')
+class admin_login(models.Model):
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
 
-        try:
-            login_data=admin_login.objects.get(email=email)
-            if login_data:
-                if login_data.password == password:
-                    request.session['user']=login_data.email
-                    request.session['password']=login_data.password
-                    return redirect('admin_view')
-        except:
-            pass
-        return render(request,'admin_login.html')
+
+    def get_admin_login_data():
+        return admin_login.objects.all()
